74dfb5d6949f708c97613a919aebafd6a4c690d7
abhinavsiwal <abhinavsiwal@gmail.com>
2023-12-09 18:18:18 +0530

    Category

---
+++ src/app/category/[id]/page.tsx
@@ -0,0 +1,57 @@
+import Categories from "@/components/Category/Categories";
+import Image from "next/image";
+export async function generateStaticParams() {
+  const data = await fetch(
+    `${process.env.backendUrl}/category/getAllCategories`
+  ).then((res) => res.json());
+
+  return data?.categories?.map((category: any) => ({
+    id: category._id,
+  }));
+}
+
+async function getSingleCategory(id: string) {
+  const res = await fetch(
+    `${process.env.backendUrl}/category/getSingleCategory/${id}`
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+
+  return data.category;
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const category = await getSingleCategory(params.id);
+
+  return (
+    <div className="w-full flex flex-col gap-16 md:py-28 ">
+      <div className="w-full px-8 mt-20 sm:px-12 md:px-20 lg:px-32 py-16 flex flex-col gap-12 md:gap-20">
+        <div className="w-full grid-cols-1  grid sm:grid-cols-2 gap-12 items-start">
+          <Image
+            src={category?.image}
+            width={600}
+            height={600}
+            alt={category?.name}
+            className="rounded-3xl cursor-pointer  "
+          />
+          <div className="w-full flex flex-col gap-8">
+            <h1 className="inter text-3xl text-primary font-semibold">
+              {category?.name}
+            </h1>
+            <div
+              className="w-full text-gray-600 inter text-base "
+              dangerouslySetInnerHTML={{
+                __html: category?.description,
+              }}
+            ></div>
+          </div>
+        </div>
+      </div>
+      <Categories id={params.id} />
+    </div>
+  );
+}
--- src/components/Category/Categories.tsx
+++ src/components/Category/Categories.tsx
@@ -1,30 +1,41 @@
-
-'use client'
+"use client";
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Image from "next/image";
+
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useAppSelector } from "@/store/redux-hooks";
 
-const Categories = () => {
+const Categories = ({ id }: any) => {
   const [swiperInstance, setSwiperInstance] = useState(null as any);
-  const { categories } = useAppSelector((state) => state.categories);
-  console.log(categories);
+  const [categories, setCategories] = useState([] as any);
+  const { categories: allCategories } = useAppSelector(
+    (state) => state.categories
+  );
+
+  useEffect(() => {
+    if (allCategories) {
+      const filteredCategories = allCategories.filter(
+        (category: any) => category?._id !== id
+      );
+      setCategories(filteredCategories);
+    }
+  }, [allCategories]);
 
   return (
-    <div className="w-full px-8 sm:px-12 md:px-20 lg:px-32 py-16 flex flex-col gap-12">
+    <div className="w-full bg-primary px-8 sm:px-12 md:px-20 lg:px-32 py-16 flex flex-col gap-12">
       <div className="w-full  flex md:flex-row flex-col justify-between gap-2 ">
         <div className="flex flex-col md:items-start items-center gap-4">
           <h6 className="spartan text-lg md:text-xl text-blue uppercase tracking-widest font-medium">
             Services
           </h6>
-          <h4 className="font-bold text-3xl md:text-4xl text-primary md:text-start text-center inter tracking-wide ">
+          <h4 className="font-bold text-3xl md:text-4xl text-primary md:text-start text-center inter tracking-wide text-white ">
             Popular Categories
           </h4>
         </div>
-        <p className="text-gray-400 inter break-words md:text-start text-center  text-md md:text-lg font-light ">
+        <p className="text-white inter break-words md:text-start text-center  text-md md:text-lg font-light ">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do usmod{" "}
           <br />
           tempor incididunt ut labore et dolore magna aliqua.
@@ -54,38 +65,36 @@ const Categories = () => {
             },
           }}
         >
-          {categories?.map((category) => {
+          {categories?.map((category: any) => {
             return (
-              <SwiperSlide key={category?._id} >
-              <div className="w-full flex flex-col items-center gap-2  ">
-                <div className="hover-effect relative flex items-center justify-center w-full ">
-                  <Image
-                    src={category?.image}
-                    width={600}
-                    height={600}
-                    alt={category?.name}
-                    className="rounded-3xl cursor-pointer   "
-                  />
+              <SwiperSlide key={category?._id}>
+                <div className="w-full flex flex-col items-center gap-2  ">
+                  <div className="hover-effect relative flex items-center justify-center w-full ">
+                    <Image
+                      src={category?.image}
+                      width={600}
+                      height={600}
+                      alt={category?.name}
+                      className="rounded-3xl cursor-pointer   "
+                    />
+                  </div>
+
+                  <p className="text-center text-white font-semibold text-lg inter">
+                    {category?.name}
+                  </p>
                 </div>
-  
-                <p className="text-center text-blue font-semibold text-lg inter">
-                {category?.name}
-                </p>
-              </div>
-            </SwiperSlide>
-           
-            )
+              </SwiperSlide>
+            );
           })}
-        
         </Swiper>
       </div>
       <div className="w-full flex justify-center gap-4">
         <ArrowBackIcon
-          className="text-primary cursor-pointer text-3xl "
+          className="text-white cursor-pointer text-3xl "
           onClick={() => swiperInstance.slidePrev()}
         />
         <ArrowForwardIcon
-          className="text-primary cursor-pointer text-3xl "
+          className="text-white cursor-pointer text-3xl "
           onClick={() => swiperInstance.slideNext()}
         />
       </div>
