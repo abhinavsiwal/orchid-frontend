afa57caa4f9fcaa031d16d713f3d98f1d2a94f02
abhinavsiwal <abhinavsiwal@gmail.com>
2024-01-04 12:07:02 +0530

    url changes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const backendUrl = "http://localhost:9000/api";
-// export const backendUrl = "https://ill-pink-jay-gown.cyclic.app/api";
+// export const backendUrl = "http://localhost:9000/api";
+export const backendUrl = "https://ill-pink-jay-gown.cyclic.app/api";
 // export const backendUrl = "";
 export const frontendUrl = "http://localhost:3000";
 
